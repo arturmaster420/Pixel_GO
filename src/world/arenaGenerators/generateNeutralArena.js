be85f81b2e7ecb8ed24ae458f12eeb0aeade5563
arturmaster420 <arturmaster420@gmail.com>
2026-03-13 13:40:45 +0200

    Delete src directory

--- src/world/arenaGenerators/generateNeutralArena.js
+++
@@ -1,8 +0,0 @@
-import { generateTemplateRoomArena } from './generateTemplateRoomArena.js';
-import { generateNeutralArena as generateNeutralArenaLegacy } from './generateNeutralArena.legacy.js';
-
-export function generateNeutralArena(input = {}) {
-  const templated = generateTemplateRoomArena(input);
-  if (templated) return templated;
-  return generateNeutralArenaLegacy(input);
-}
