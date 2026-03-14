6a2439e9fad340878c27c525bf97c97ec3df4300
arturmaster420 <arturmaster420@gmail.com>
2026-03-14 05:48:40 +0200

    Delete src directory

--- src/world/arenaGenerators/generateDarkArena.js
+++
@@ -1,8 +0,0 @@
-import { generateTemplateRoomArena } from './generateTemplateRoomArena.js';
-import { generateDarkArena as generateDarkArenaLegacy } from './generateDarkArena.legacy.js';
-
-export function generateDarkArena(input = {}) {
-  const templated = generateTemplateRoomArena(input);
-  if (templated) return templated;
-  return generateDarkArenaLegacy(input);
-}
