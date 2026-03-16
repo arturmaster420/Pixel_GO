283293165cab15293573db6e5f87b080d3365957
arturmaster420 <arturmaster420@gmail.com>
2026-03-16 21:23:54 +0200

    Delete src/src directory

--- src/src/world/arenaGenerators/generateElectricArena.js
+++
@@ -1,8 +0,0 @@
-import { generateTemplateRoomArena } from './generateTemplateRoomArena.js';
-import { generateElectricArena as generateElectricArenaLegacy } from './generateElectricArena.legacy.js';
-
-export function generateElectricArena(input = {}) {
-  const templated = generateTemplateRoomArena(input);
-  if (templated) return templated;
-  return generateElectricArenaLegacy(input);
-}
