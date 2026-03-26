4e8b36f9ccd07331b6e5490ef3790c93026d25af
arturmaster420 <arturmaster420@gmail.com>
2026-03-26 13:09:24 +0200

    Delete src/core directory

--- src/core/hubBuild.js
+++
@@ -1,6 +0,0 @@
-export * from "./hub/hubShared.js";
-export * from "./hub/hubBuildState.js";
-export * from "./hub/hubResources.js";
-export * from "./hub/hubGear.js";
-export * from "./hub/hubLoot.js";
-export * from "./hub/hubApplyToPlayer.js";
